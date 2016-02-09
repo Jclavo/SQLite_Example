0cede72eacf2f469a75965d8817bce4c57079f25
JClavo <jclavotafur@gmail.com>
2016-02-08 19:11:17 -0500

    FINAL example

--- app/src/main/java/com/example/jclavo/sqlite_example/MainActivity.java
+++ app/src/main/java/com/example/jclavo/sqlite_example/MainActivity.java
@@ -5,48 +5,90 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText txtProductName;
+    EditText txtProductQuantity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void addProduct (View view) {
+
+        int quantity;
+        Product product;
+
+        txtProductName = (EditText) findViewById(R.id.txtProductName);
+        txtProductQuantity = (EditText) findViewById(R.id.txtProductQuantity);
+
+        MyDBHandler dbHandler = new MyDBHandler(this, null, null, 1);
+
+        quantity = Integer.parseInt(txtProductQuantity.getText().toString());
+
+        product = new Product(txtProductName.getText().toString(), quantity);
+
+        dbHandler.addProduct(product);
+
+        mensaje("Adition Product");
+        txtProductName.setText("");
+        txtProductQuantity.setText("");
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void lookupProduct (View view) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        txtProductName = (EditText) findViewById(R.id.txtProductName);
+        txtProductQuantity = (EditText) findViewById(R.id.txtProductQuantity);
+
+        MyDBHandler dbHandler = new MyDBHandler(this, null, null, 1);
+
+        Product product = dbHandler.findProduct(txtProductName.getText().toString());
+
+        if (product != null) {
+            //idView.setText(String.valueOf(product.getId()));
+            Log.i("Id product",String.valueOf(product.getId()));
+            txtProductQuantity.setText(String.valueOf(product.getQuantity()));
+        } else {
+
+            mensaje("No Match Found");
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    public void deleteProduct (View view) {
+
+        boolean result;
+
+        txtProductName = (EditText) findViewById(R.id.txtProductName);
+        txtProductQuantity = (EditText) findViewById(R.id.txtProductQuantity);
+
+        MyDBHandler dbHandler = new MyDBHandler(this, null,null, 1);
+
+        result = dbHandler.deleteProduct(txtProductName.getText().toString());
+
+        if (result)
+        {
+            mensaje("Record Deleted");
+            txtProductName.setText("");
+            txtProductQuantity.setText("");
+        }
+        else
+            mensaje("No Match Found");
+    }
+
+    public void mensaje(String mensaje){
+        Toast.makeText(getApplicationContext(), mensaje, Toast.LENGTH_SHORT).show();
     }
 }
